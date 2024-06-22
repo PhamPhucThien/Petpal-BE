538e6c3475abda52abd68300af0e11754765b0a4
dell <dell@DESKTOP-L9PUH3P>
2024-06-22 12:40:19 +0700

    Update DTO Request Response

---
+++ CapstoneProject.DTO/Request/Account/CreateUserRequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapstoneProject.DTO.Request.Account
+{
+    public class CreateUserRequest
+    {
+        [Required(ErrorMessage = "User ID is required")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "UserName is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        public string FullName { get; set; }
+        [Required(ErrorMessage = "Address is required")]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "PhoneNumber is required")]
+        public string PhoneNumber { get; set; }
+        //[Required(ErrorMessage = "ProfileImage is required")]
+        public string ProfileImage { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+    }
+}
---
+++ CapstoneProject.DTO/Request/Service/CreateServiceRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapstoneProject.DTO.Request.Service
+{
+    public class CreateServiceRequest
+    {
+
+        [Required(ErrorMessage = "Service ID is required")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Service Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Base Price is required")]
+        public double BasePrice { get; set; }
+        //[Required(ErrorMessage = "Is Required")]
+        public bool IsRequired { get; set; }
+    }
+}
