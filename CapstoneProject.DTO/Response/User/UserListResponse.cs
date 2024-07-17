2d2c7c5b876a5d0ed914a8aec428b4dcd803d465
thanhnh20 <huuthanh10010308@gmail.com>
2024-07-18 05:05:19 +0700

    add get all,  get by id, create and update for user

---
+++ CapstoneProject.DTO/Response/User/UserListResponse.cs
@@ -0,0 +1,10 @@
+﻿using CapstoneProject.DTO.Request;
+
+using CapstoneProject.DTO.Request;
+using CapstoneProject.DTO.Response.User;
+
+public class UserListResponse
+{
+    public List<UserResponse>? List { get; set; }
+    public Paging Paging { get; set; } = new Paging();
+}
