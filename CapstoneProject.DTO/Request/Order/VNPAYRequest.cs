972f3d82130f64cbce6435a9df962ba8364421f1
PhamPhucThien <Tungn5560@gmail.com>
2024-08-06 08:38:40 +0700

    fix vnpay return

--- CapstoneProject.DTO/Request/Order/VNPAYRequest.cs
+++ CapstoneProject.DTO/Request/Order/VNPAYRequest.cs
@@ -8,11 +8,17 @@ namespace CapstoneProject.DTO.Request.Order
 {
     public class VNPAYRequest
     {
-        public string? VnpSecureHash { get; set; }
-        public string? VnpOrderInfo { get; set; }
         public string? VnpAmount { get; set; }
-        public string? VnpTransactionNo { get; set; }
+        public string? VnpBankCode { get; set; }
+        public string? VnpBankTranNo { get; set; }
         public string? VnpCardType { get; set; }
+        public string? VnpOrderInfo { get; set; }
+        public string? VnpPayDate { get; set; }
+        public string? VnpResponseCode { get; set; }
+        public string? VnpTmnCode { get; set; }
+        public string? VnpTransactionNo { get; set; }
         public string? VnpTransactionStatus { get; set; }
+        public string? VnpTxnRef { get; set; }
+        public string? VnpSecureHash { get; set; }
     }
 }
